598cdeb98aa1d823ba7b8b1b18361a3d6dfee9c7
joshuazg <joshuazhiguang@hotmail.com>
2023-12-18 23:32:50 +0800

    done

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <BrowserRouter basename="enterprises/goscoot/GoScootMY/react-website">
+      <BrowserRouter basename="enterprises/goscoot/react-website">
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<Login setToken={setToken} />} />
